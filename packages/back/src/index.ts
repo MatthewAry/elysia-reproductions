49911f63f1d36213c659918bb7768053d479f0ca
Matthew Ary <matthew.ary@symplsoft.io>
2024-03-05 06:43:16 -0800

    Adding more to reproduction

--- packages/back/src/index.ts
+++ packages/back/src/index.ts
@@ -4,11 +4,18 @@ import serverTiming from "@elysiajs/server-timing";
 const app = new Elysia()
   .use(serverTiming())
   .guard({
-    beforeHandle({ error }) {
-        // Hangs when running in dev mode. Works in production mode.
-        return error(403, "You should get this error.")
+    beforeHandle: async ({ error }) => {
+      // Add Synthetic delay
+      const result = await new Promise((resolve) => {
+        setTimeout(resolve, 1000)
+        resolve(true)
+      });
+      if (result) {
+        return error(403, "You should get this error.");
       }
-    },
+      // Hangs when running in dev mode. Works in production mode.
+    }
+  },
     (app) => app.get("/reject", () => "You shouldn't be here!")
   )
   .listen(3000);
