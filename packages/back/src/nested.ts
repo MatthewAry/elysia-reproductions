f3b7445070610558bae38c6f29564eb33f43345a
Matthew Ary <matthew.ary@symplsoft.io>
2024-03-05 17:08:32 -0800

    Typo fix

--- packages/back/src/nested.ts
+++ packages/back/src/nested.ts
@@ -2,4 +2,4 @@ import { Elysia, t } from "elysia";
 
 export const nested = new Elysia({ prefix: '/level1/level2' })
   .get('', () => 'Level 2')
-  .get("/:id", ({ params: { id }}) => `You are in the identified route! ${id}`);` `
+  .get("/:id", ({ params: { id }}) => `You are in the identified route! ${id}`);
